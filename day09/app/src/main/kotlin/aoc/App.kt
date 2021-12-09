d1d6f705a1008ea4c2f86e972ecc163480fbb171
Gustav Aaro <gustav.aaro@gmail.com>
2021-12-09 12:55:23 +0100

    Small optimizations to day 09

--- day09/app/src/main/kotlin/aoc/App.kt
+++ day09/app/src/main/kotlin/aoc/App.kt
@@ -9,12 +9,11 @@ fun main() {
 
 data class Point(val x: Int, val y: Int)
 operator fun Array<Array<Int>>.get(point: Point) = this[point.x][point.y]
-operator fun Array<Array<Int>>.set(point: Point, value: Int) {
-    this[point.x][point.y] = value
-}
+operator fun Array<Array<Int>>.set(point: Point, value: Int) { this[point.x][point.y] = value }
 
 fun getSolutionPart1(input: Array<Array<Int>>): Int = getLowPoints(input).map { input[it] + 1 }.sum()
-fun getSolutionPart2(input: Array<Array<Int>>): Int = getLowPoints(input).map { getBasins(it, input) }.sortedByDescending { it.size }.subList(0,3).fold(1) { acc, set -> acc * set.size }
+
+fun getSolutionPart2(input: Array<Array<Int>>): Int = getLowPoints(input).map { getBasins(it, input).size }.sortedByDescending { it }.subList(0,3).fold(1) { acc, size -> acc * size }
 
 fun getLowPoints(input: Array<Array<Int>>): List<Point> = input.flatMapIndexed { row, values -> values.mapIndexedNotNull { col, height -> if (getAdjacentPoints(Point(row, col), input).none { input[it] <= height }) Point(row, col) else null } }
 
@@ -26,10 +25,10 @@ fun getBasins(point: Point, map: Array<Array<Int>>, basins: MutableSet<Point> = 
 }
 
 fun getAdjacentPoints(point: Point, map: Array<Array<Int>>): List<Point> {
-    val adjacentPoints = mutableListOf<Point>()
-    if (point.x > 0) adjacentPoints.add(Point(point.x - 1, point.y))
-    if (point.y > 0) adjacentPoints.add(Point(point.x, point.y - 1))
-    if (point.x < map.size - 1) adjacentPoints.add(Point(point.x + 1, point.y))
-    if (point.y < map.first().size - 1) adjacentPoints.add(Point(point.x, point.y + 1))
-    return adjacentPoints
+    val adjacentPoints = Array<Point?>(4){ null }
+    if (point.x > 0) adjacentPoints[0] = Point(point.x - 1, point.y)
+    if (point.y > 0) adjacentPoints[1] = Point(point.x, point.y - 1)
+    if (point.x < map.size - 1) adjacentPoints[2] = Point(point.x + 1, point.y)
+    if (point.y < map[0].size - 1) adjacentPoints[3] = Point(point.x, point.y + 1)
+    return adjacentPoints.filterNotNull()
 }
